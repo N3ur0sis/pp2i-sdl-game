8227396678bb72c2ed6406bfbe2e6d82968531fd
Aymeric ROBERT <aymerob@gmail.com>
2024-02-17 19:19:09 +0100

    first opengl implementation

--- src/main.c
+++ src/main.c
@@ -4,8 +4,9 @@
 // This is just a basic program to make sure the build system works for everyone      //
 //************************************************************************************//
 
-
+#define GL_GLEXT_PROTOTYPES
 #include <SDL2/SDL.h>
+#include <SDL_opengl.h>
 #include <stdbool.h>
 
 //Screen dimensions
@@ -18,13 +19,16 @@ int main()
     // Initialize SDL
     SDL_Init(SDL_INIT_VIDEO);
     // Create window
-    SDL_Window *window = SDL_CreateWindow("Basic C SDL project",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
-    // Create renderer
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Window *window = SDL_CreateWindow("OpenGL Renderer",
+                         SDL_WINDOWPOS_CENTERED, 
+                         SDL_WINDOWPOS_CENTERED, 
+                         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    
     // Exit flag
     bool quit = false;
     // Render Loop
@@ -39,14 +43,13 @@ int main()
             quit = true;
         }
         // Initialize renderer color white for the background
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        glClearColor(1, 0, 1, 1);
         // Clear screen
-        SDL_RenderClear(renderer);
-        // Update screen
-        SDL_RenderPresent(renderer);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow( window );
     }
     //When quit is true we clean up  some shit
-    SDL_DestroyRenderer(renderer);
+    SDL_DestroyRenderer(context);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
