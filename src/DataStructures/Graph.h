6abc81eb05a4b426e71e4e7944b2bbfc60bdc81c
DAVILLE Victor <victor.daville@telecomnancy.eu>
2024-02-23 09:18:56 +0100

    added first graph iteration (not working)

--- src/DataStructures/Graph.h
+++ src/DataStructures/Graph.h
@@ -5,4 +5,32 @@
 #ifndef PP2I_GRAPH_H
 #define PP2I_GRAPH_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "Data.h"
+#include "Vector.h"
+
+typedef struct _Graph Graph;
+typedef struct _Data Data;
+
+struct _Graph {
+    Vector** adjacencyList;
+    int size;
+    Type type;
+    Vector* data;
+};
+
+
+Graph* createGraph(Type type);
+
+int sizeGraph(Graph* graph);
+
+void addVertex(Graph* graph, Data* data);
+
+void addEdge(Graph* graph, int vertex1, int vertex2);
+
+bool emptyGraph(Graph* graph);
+
+void destroyGraph(Graph* graph);
+
 #endif //PP2I_GRAPH_H
