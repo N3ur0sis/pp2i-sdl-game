bcfb985653de43db328c6ff54016f10bd1ba8215
DAVILLE Victor <victor.daville@telecomnancy.eu>
2024-02-23 09:50:18 +0100

    bug fixes

--- src/Engine/Loader/ModelLoader.c
+++ src/Engine/Loader/ModelLoader.c
@@ -13,18 +13,22 @@ bool ModelLoader(char* path, Vector** vertices, Vector** normals, Vector** uvs, 
     char line[256];
     while (fgets(line, sizeof(line), f)) {
         if (line[0] == 'v' && line[1] == ' ') {
-            Vector* vertex = createVector();
+            Vector* vertex = createVector(FLOAT);
             reserve(vertex, 3);
             float x, y, z;
             sscanf(line, "v %f %f %f", &x, &y, &z);
-            Data data;
-            data.f = x;
+            Data* data = createFloatData(x);
             push_back(vertex, data);
-            data.f = y;
+            data = createFloatData(y);
             push_back(vertex, data);
-            data.f = z;
+            data = createFloatData(z);
             push_back(vertex, data);
-            push_back(*vertices, vertex);
+            push_back(*vertices, createVectorData(vertex));
         }
     }
+    normals[0] = createVector(FLOAT);
+    uvs[0] = createVector(FLOAT);
+    indices[0] = createVector(INT);
+    fclose(f);
+    return true;
 }
