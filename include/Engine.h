a95241b07122ec92a2fc4cbf68d3677ebf96ba18
Aymeric Robert <aymerob@gmail.com>
2024-03-27 12:06:37 +0100

    feat(core): created an Application class in  Engine

--- include/Engine.h
+++ include/Engine.h
@@ -1,6 +1,21 @@
 #pragma once
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
+#include <Window.h>
+#include <Time.h>
+#include <Controls.h>
+#include <stdbool.h>
 
-void GL_Init(void);
-void GL_Quit(void);
+typedef struct _Application{
+    Window* window;
+    Time* time;
+    SDL_Event* event;
+    bool running;
+}Application;
+
+Application* ApplicationCreate(int width, int height, const char* title);
+void StartFrame(Application* game);
+void EndFrame(Application* game);
+
+void EngineInit(void);
+void EngineQuit(void);
--- src/Engine/Core/Controls.c
+++ src/Engine/Core/Controls.c
@@ -1,22 +1,25 @@
 #include <Controls.h>
+#include <math.h>
 
 
 //State of Mouse and keyboard input
-char keyState[255] = { 0 };
+char keyState[NB_KEYS] = { 0 };
 float mousePos[6] = {0}; // 0->posX 1->posY 2->lastX 3->lastY 4->wheely ->5 lastWheelY
 char mouseState[5] = { 0 }; // mouse button state
 
 
-void processInput(SDL_Event* e,  bool* done){
+void processInput(SDL_Event* e,  bool* running){
+
+	
 	switch (e->type){
 		case SDL_QUIT:
-			*done = true;
+			*running = false;
 			break;
 		case SDL_KEYDOWN:
-			handleKeyBoardEvent(*e);
+			handleKeyBoardEventDown(*e);
 			break;
 		case SDL_KEYUP:
-			handleKeyBoardEvent(*e);
+			handleKeyBoardEventUp(*e);
 			break;
 		case SDL_MOUSEMOTION:
 			mousePos[0] = e->motion.x;
@@ -33,9 +36,26 @@ void processInput(SDL_Event* e,  bool* done){
 	}
 }
 
-void handleKeyBoardEvent(SDL_Event e){
-	keyState[e.key.keysym.sym] = e.key.state;
+void handleKeyBoardEventDown(SDL_Event e){
+	if (e.key.keysym.scancode == SDL_SCANCODE_LSHIFT || e.key.keysym.scancode == SDL_SCANCODE_RSHIFT){  
+		keyState[255] = 1;
+	} else if (e.key.keysym.scancode == SDL_SCANCODE_TAB) {
+		keyState[256] = 1;
+	} else if (e.key.keysym.sym < 255) {
+		keyState[e.key.keysym.sym] = e.key.state;
+	}
 }
+
+void handleKeyBoardEventUp(SDL_Event e){
+	if (e.key.keysym.scancode == SDL_SCANCODE_LSHIFT || e.key.keysym.scancode == SDL_SCANCODE_RSHIFT){  
+		keyState[255] = 0;
+	} else if (e.key.keysym.scancode == SDL_SCANCODE_TAB) {
+		keyState[256] = 0;
+	} else if (e.key.keysym.sym < 255) {
+		keyState[e.key.keysym.sym] = e.key.state;
+	}
+}
+
 
 void handleMouseButtonEvent(SDL_Event e){
 	mouseState[e.button.button] = e.button.state;
@@ -60,4 +80,3 @@ int getMousePosition(int coord){
 void setMousePosition(int coord, float value){
 	mousePos[coord] = value;
 }
-
