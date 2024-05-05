229b9c89276508f5e7b2b3f81dd4d038b3351942
DAVILLE Victor <victor.daville@telecomnancy.eu>
2024-05-05 23:14:37 +0200

    fix?

--- src/Engine/Scene/Menu.c
+++ src/Engine/Scene/Menu.c
@@ -1,23 +1,5 @@
 #include "Menu.h"
 
 void renderPauseMenu(Application* game) {
-    if (TTF_Init() == -1) {
-        printf("TTF_Init: %s\n", TTF_GetError());
-        exit(2);
-    }
-
-    TTF_Font* font = TTF_OpenFont("assets/fonts/OpenSans-Regular.ttf", 24);
-    if (font == NULL) {
-        printf("TTF_OpenFont: %s\n", TTF_GetError());
-        exit(2);
-    }
-
     (void) game;
-
-    SDL_Color color = {255, 255, 255, 255}; 
-    SDL_Surface* surface = TTF_RenderText_Solid(font, "Game Paused", color);
-
-    SDL_FreeSurface(surface);
-    TTF_CloseFont(font);
-    TTF_Quit();
 }
