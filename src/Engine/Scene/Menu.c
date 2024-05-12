280a7ca8a4237145bf5c12fa6d91c79f934a0102
DAVILLE Victor <victor.daville@telecomnancy.eu>
2024-05-12 16:03:03 +0200

    added sdl_ttf support for mac

--- src/Engine/Scene/Menu.c
+++ src/Engine/Scene/Menu.c
@@ -1,5 +1,7 @@
 #include "Menu.h"
 
 void renderPauseMenu(Application* game) {
+    TTF_Font* font = TTF_OpenFont("assets/fonts/Roboto-Regular.ttf", 24);
+    SDL_Color color = {255, 255, 255, 255};
     (void) game;
 }
