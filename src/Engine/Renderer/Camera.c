35b6bc2eb08da074b7a28bdcf1414bcb277bc2d2
Victor Davillé <enkodo@moi>
2024-05-26 03:09:05 +0200

    Audio camera and collier header doc

--- src/Engine/Renderer/Camera.c
+++ src/Engine/Renderer/Camera.c
@@ -1,9 +1,25 @@
+/*
+* @file Camera.c
+* @brief Implementations of camera functions.
+*/
+
 #include <Camera.h>
 
 bool firstLook = true;
 bool firstMove = true;
 
-//Camera constructor
+/*
+* @brief Creates a new Camera instance.
+*
+* This function creates a new Camera instance.
+*
+* @param posX The x position of the camera.
+* @param posY The y position of the camera.
+* @param posZ The z position of the camera.
+* @param width The width of the camera.
+* @param height The height of the camera.
+* @return Pointer to the created Camera instance or NULL on failure.
+*/
 Camera* camera_create(float posX, float posY, float posZ, float width, float height){
     Camera* camera = (Camera*)malloc(sizeof(Camera));
     camera->Position[0] = posX;
@@ -22,14 +38,27 @@ Camera* camera_create(float posX, float posY, float posZ, float width, float hei
     return camera;
 }
 
-
+/**
+ * @brief Gets the view matrix of the camera.
+ * 
+ * This function gets the view matrix of the camera.
+ * 
+ * @param camera The camera to get the view matrix from.
+ * @param view The view matrix.
+ */
 void getViewMatrix(Camera* camera, vec4* view){
     vec3 center;
     glm_vec3_add(camera->Position, camera->Front, center);
     glm_lookat(camera->Position, center , camera->Up, view);
 }
 
-
+/*
+* @brief Updates the camera vectors.
+*
+* This function updates the camera vectors.
+*
+* @param camera The camera to update.
+*/
 void updateCameraVectors(Camera* camera){
         // calculate the new Front vector
         vec3 front;
