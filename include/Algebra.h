03640f81437aaf9b66d8afcfed001a2e9cc771da
Victor Davillé <enkodo@moi>
2024-05-26 02:56:35 +0200

    doxygen algebra, animation and animator headers

--- include/Algebra.h
+++ include/Algebra.h
@@ -1,7 +1,40 @@
+/*
+* @file Algebra.h
+* @brief Implementations of algebra functions.
+*
+* This file contains the implementations of algebra functions.
+*/
+
 #pragma once
 #include <assimp/cimport.h>
 #include <cglm/cglm.h>
 
+/**
+ * @brief Converts an assimp vector to a glm vector.
+ * 
+ * This function converts an assimp vector to a glm vector.
+ * 
+ * @param vec The glm vector.
+ * @param ai_vec The assimp vector.
+ */
 void AssimpVec3(vec3 vec, struct aiVector3D ai_vec);
+
+/**
+ * @brief Converts an assimp quaternion to a glm quaternion.
+ * 
+ * This function converts an assimp quaternion to a glm quaternion.
+ * 
+ * @param q The glm quaternion.
+ * @param ai_q The assimp quaternion.
+ */
 void AssimpQuat(versor q, struct aiQuaternion ai_q);
+
+/**
+ * @brief Converts an assimp matrix to a glm matrix.
+ * 
+ * This function converts an assimp matrix to a glm matrix.
+ * 
+ * @param mat The glm matrix.
+ * @param ai_mat The assimp matrix.
+ */
 void AssimpMat4(mat4 mat, struct aiMatrix4x4 ai_mat);
--- include/Animation.h
+++ include/Animation.h
@@ -1,20 +1,89 @@
+/*
+* @file Animation.h
+* @brief Implementations of animation functions.
+*
+* This file contains the implementations of animation functions.
+*/
+
 #pragma once
 #include <Model.h>
 
+/**
+ * @brief Structure representing a node in the skeleton.
+ * 
+ * This structure represents a node in the skeleton.
+*/
 typedef struct Animation {
-    Node* root_node;
-    mat4 parent_mat;
-    mat4* bone_mats;
-    mat4* bone_anim_mats;
-    float anim_dur;
-    float anim_ticks;
-    char * name;
+    Node* root_node;    /**< Root node of the animation. */
+    mat4 parent_mat;    /**< Parent transformation matrix */
+    mat4* bone_mats;    /**< Array of bone matrices */
+    mat4* bone_anim_mats;   /**< Array of bone animation matrices */
+    float anim_dur;     /**< Duration of the animation */
+    float anim_ticks;   /**< Ticks per second of the animation */
+    char * name;        /**< Name of the animation */
 } Animation;
 
+/**
+ * @brief Creates a new Animation instance.
+ * 
+ * @param path The path to the model file.
+ * @param model The model to animate.
+ * @param name The name of the animation.
+ * @return Pointer to the created Animation instance or NULL on failure.
+ */
 Animation* AnimationCreate(char* path, Model* model, char* name);
+
+/**
+ * @brief Calculates the bone transformation.
+ * 
+ * This function calculates the bone transformation.
+ * 
+ * @param node The node to calculate the transformation for.
+ * @param anim_time The time of the animation.
+ * @param parent_mat The parent matrix.
+ * @param bones The array of bone matrices.
+ * @param bone_anim_mats The array of bone animation matrices.
+ */
 void CalculateBoneTransformation(Node* node, float anim_time, mat4 parent_mat, mat4* bones,mat4* bone_anim_mats);
 
+/**
+ * @brief Finds a node in the skeleton.
+ * 
+ * This function finds a node in the skeleton.
+ * 
+ * @param root The root node.
+ * @param name The name of the node.
+ * @return Pointer to the found node or NULL if not found.
+ */
 Node* NodeFind(Node* root, const char* name);
+
+/**
+ * @brief Imports a node from an assimp node.
+ * 
+ * This function imports a node from an assimp node.
+ * 
+ * @param ai_node The assimp node.
+ * @param skel_node The skeleton node.
+ * @param bone_count The number of bones.
+ * @param bone_names The array of bone names.
+ * @return 0 on success, 1 on failure.
+ */
 int NodeImport(const struct aiNode* ai_node, Node** skel_node, size_t bone_count, char bone_names[][256]);
+
+/**
+ * @brief Deletes a node.
+ * 
+ * This function deletes a node.
+ * 
+ * @param node The node to delete.
+ */
 void NodeDelete(Node* node);
+
+/**
+ * @brief Deletes an animation.
+ * 
+ * This function deletes an animation.
+ * 
+ * @param animation The animation to delete.
+ */
 void AnimationDelete(Animation* animation);
