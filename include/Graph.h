833edfcba2536ea963745d370c579254dd636409
Victor Davillé <enkodo@moi>
2024-05-26 19:08:50 +0200

    doc chapter 2

--- include/Graph.h
+++ include/Graph.h
@@ -1,6 +1,8 @@
-//
-// Created by Victor Davillé on 19/02/2024.
-//
+/**
+ * @file Graph.h
+ * @brief Header file for the graph component.
+ * @author VD
+*/
 
 #ifndef PP2I_GRAPH_H
 #define PP2I_GRAPH_H
@@ -13,24 +15,77 @@
 typedef struct _Graph Graph;
 typedef struct _Data Data;
 
+/**
+ * @brief Struct representing a graph.
+ * 
+ * This struct represents a graph.
+*/
 struct _Graph {
-    Vector** adjacencyList;
-    int size;
-    Type type;
-    Vector* data;
+    Vector** adjacencyList; /**< Adjacency list of the graph. */
+    int size;               /**< Size of the graph. */
+    Type type;              /**< Type of the graph. */
+    Vector* data;           /**< Vector of data. */
 };
 
 
+/**
+ * @brief Function to create a graph.
+ * 
+ * This function creates a graph.
+ * 
+ * @param type Type of the graph.
+ * @return Pointer to the graph.
+ */
 Graph* createGraph(Type type);
 
+/**
+ * @brief Function to get the size of the graph.
+ * 
+ * This function gets the size of the graph.
+ * 
+ * @param graph Pointer to the graph.
+ * @return Size of the graph.
+ */
 int sizeGraph(Graph* graph);
 
+/**
+ * @brief Function to add a vertex to the graph.
+ * 
+ * This function adds a vertex to the graph.
+ * 
+ * @param graph Pointer to the graph.
+ * @param data Pointer to the data.
+ */
 void addVertex(Graph* graph, Data* data);
 
+/**
+ * @brief Function to add an edge to the graph.
+ * 
+ * This function adds an edge to the graph.
+ * 
+ * @param graph Pointer to the graph.
+ * @param vertex1 Index of the first vertex.
+ * @param vertex2 Index of the second vertex.
+ */
 void addEdge(Graph* graph, int vertex1, int vertex2);
 
+/**
+ * @brief Function to empty the graph.
+ * 
+ * This function empties the graph.
+ * 
+ * @param graph Pointer to the graph.
+ * @return Boolean value indicating if the graph is empty.
+ */
 bool emptyGraph(Graph* graph);
 
+/**
+ * @brief Function to destroy the graph.
+ * 
+ * This function destroys the graph.
+ * 
+ * @param graph Pointer to the graph.
+ */
 void destroyGraph(Graph* graph);
 
 #endif //PP2I_GRAPH_H
